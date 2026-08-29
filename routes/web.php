24cadbe65f059649e5f6442cd607bb178952bd42
Uploadfilesetiem <sallru529@gmail.com>
2026-08-29 19:56:18 +0700

    fix define stok and laporan routes

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KasirController;
 
+// Route Kasir
 Route::get('/', [KasirController::class, 'index'])->name('kasir.index');
 Route::get('/kasir', [KasirController::class, 'index'])->name('kasir.index');
 Route::post('/kasir/store', [KasirController::class, 'store'])->name('kasir.store');
 Route::get('/kasir/struk/{id}', [KasirController::class, 'struk'])->name('kasir.struk');
 
+// Route Stok & Laporan Fallback
+Route::get('/stok', function() {
+    return redirect()->route('kasir.index');
+})->name('stok.index');
+
+Route::get('/laporan', function() {
+    return redirect()->route('kasir.index');
+})->name('laporan.index');
